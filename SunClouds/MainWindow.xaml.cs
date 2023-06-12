820446e109d0b8bfd8ffd6463e2e875f33944f7c
aidar516 <isip_a.r.valiullin@mpt.ru>
2023-06-12 17:04:12 +0300

    FirstTheme

--- SunClouds/MainWindow.xaml.cs
+++ SunClouds/MainWindow.xaml.cs
@@ -15,14 +15,33 @@ using System.Windows.Shapes;
 
 namespace SunClouds
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
