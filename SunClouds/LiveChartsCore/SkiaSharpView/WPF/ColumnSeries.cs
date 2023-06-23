72dd08e3f3438384dec06b5555f1ac612c8d4c14
aidar516 <isip_a.r.valiullin@mpt.ru>
2023-06-23 16:43:57 +0300

    DiagramsButton

---
+++ SunClouds/LiveChartsCore/SkiaSharpView/WPF/ColumnSeries.cs
@@ -0,0 +1,10 @@
+﻿using LiveCharts;
+
+namespace LiveChartsCore.SkiaSharpView.WPF
+{
+    internal class ColumnSeries
+    {
+        public string Title { get; set; }
+        public ChartValues<double> Values { get; set; }
+    }
+}
---
+++ SunClouds/LiveChartsCore/SkiaSharpView/WPF/LineSeries.cs
@@ -0,0 +1,10 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+
+namespace LiveChartsCore.SkiaSharpView.WPF
+{
+    internal class LineSeries
+    {
+        public ChartValues<ObservablePoint> Values { get; set; }
+    }
+}
